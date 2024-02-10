755c4ebaa14c631b6d3a4e687c3d15811b63ff4a
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-10 13:31:34 +0400

    functions

---
+++ Types/functions.ts
@@ -0,0 +1,23 @@
+// function square(num) {
+//     num.toupperCase()
+//     num()
+//     return num * num
+// }
+
+// square(3)
+// square(true)
+// square('test')
+
+function square(num: number) {
+    return num * num
+}
+
+square(3)
+square('User')
+
+function greet(person: string) {
+    return `Hi there, ${person}`
+}
+
+greet(3)
+greet('User')
