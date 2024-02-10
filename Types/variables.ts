93643a1c43d4ece2917d826d84a274137aaa3cb5
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-10 13:02:05 +0400

    number variable

--- Types/variables.ts
+++ Types/variables.ts
@@ -3,3 +3,9 @@ movieTitle = 'Thor'
 movieTitle = 5
 movieTitle.name = 'X-men'
 movieTitle.toupperCase
+
+let numCat: number = 7;
+numCat += 1
+numCat = 'test'
+numCat.name = 'X-men'
+numCat.toupperCase
