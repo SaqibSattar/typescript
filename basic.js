77699660efb335986a9236d2021296fa73d2ef7c
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-10 12:52:13 +0400

    first commit

---
+++ basic.js
@@ -0,0 +1,4 @@
+var pi = 3.14;
+console.log('Hello');
+console.log(Math.round(3.3434343));
+console.log('Bye');
---
+++ basic.ts
@@ -0,0 +1,5 @@
+let pi: number = 3.14
+console.log('Hello')
+console.log(Math.round(3.3434343))
+console.log('Bye')
+
