8a81b036673873da264f0a51c344b63c96690c4f
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-10 13:03:04 +0400

    boolean variables

--- Types/variables.ts
+++ Types/variables.ts
@@ -9,3 +9,10 @@ numCat += 1
 numCat = 'test'
 numCat.name = 'X-men'
 numCat.toupperCase
+
+
+let gameOver: boolean = false;
+gameOver = true
+gameOver = 'test'
+gameOver.name = 'X-men'
+gameOver.toupperCase
