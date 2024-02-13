e28d42c6fe5f37465f4bc131364c1c429f491e04
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-13 11:16:21 +0400

    return type

--- Types/functions.ts
+++ Types/functions.ts
@@ -8,24 +8,33 @@
 // square(true)
 // square('test')
 
-function square(num: number) {
-    return num * num
+// function square(num: number) {
+//     return num * num
+// }
+
+// square(3)
+// square('User')
+
+// function greet(person: string) {
+//     return `Hi there, ${person}`
+// }
+
+// greet(3)
+// greet('User')
+
+
+// function userGreet(person: string = 'user') {
+//     return `Hi there, ${person}`
+// }
+
+// userGreet()
+// userGreet('John')
+
+
+function add(num1: number, num2: number) {
+    return num1 + num2
 }
 
-square(3)
-square('User')
-
-function greet(person: string) {
-    return `Hi there, ${person}`
+function multiply(num1: number, num2: number): number {
+    return num1 * num2
 }
-
-greet(3)
-greet('User')
-
-
-function userGreet(person: string = 'user') {
-    return `Hi there, ${person}`
-}
-
-userGreet()
-userGreet('John')
