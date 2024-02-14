2be7287d9cf4f8393e949fb3e9aef7be026f57e2
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-14 10:39:13 +0400

    excess properties

--- Types/objects.ts
+++ Types/objects.ts
@@ -9,3 +9,8 @@ let coordinate: { x: number, y: number } = { x: 34, y: 2 };
 function randomCoordinate(): { x: number, y: number} {
     return { x: Math.random(), y: Math.random() }
 }
+
+
+printName({first: 'Mike', last: 'Wick', age: 233})
+const singer = {first: 'Mike', last: 'Wick', age: 233}
+printName(singer)
