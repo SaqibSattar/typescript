1592972fa9c30ae126fc2823f47e58f9401e505d
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-15 17:20:48 +0400

    readonly and optional properties in interface

--- Types/interfaces.ts
+++ Types/interfaces.ts
@@ -4,3 +4,15 @@ interface Point {
 }
 
 const pt: Point = {x: 12, y:34}
+
+interface Person {
+    readonly id: number;
+    first: string;
+    last: string;
+    nickname?: string;
+}
+
+const thomas: Person = { first: 'Thomas', last: 'Hardy', id: 12};
+
+thomas.first = 'test';
+thomas.id = 23
