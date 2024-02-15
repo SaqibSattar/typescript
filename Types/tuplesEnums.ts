1ace99e4b4a442afaca00c80ed60d8fa0e2515e1
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-15 15:49:55 +0400

    some exercise on tuples

--- Types/tuplesEnums.ts
+++ Types/tuplesEnums.ts
@@ -1,1 +1,11 @@
 const color: [number, number, number] = [255, 0, 255]
+
+type HTTPResponse = [number, string];
+
+// const goodRes: HTTPResponse = ['Ok', 3]
+const goodRes: HTTPResponse = [2, 'Ok']
+// goodRes[0] = 'test'
+// goodRes.push(3)
+// goodRes.pop()
+// goodRes.pop()
+// goodRes.pop()
