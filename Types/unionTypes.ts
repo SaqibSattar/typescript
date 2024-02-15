f1590a5be7e8b203964059b520e2f671cabe7076
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-15 11:40:09 +0400

    type narrowing with union types

--- Types/unionTypes.ts
+++ Types/unionTypes.ts
@@ -1,17 +1,31 @@
-let age: number | string = 11
-age = 23;
-age = '23'
+// let age: number | string = 11
+// age = 23;
+// age = '23'
 
 
-type Point = {
-    x: number,
-    y: number
+// type Point = {
+//     x: number,
+//     y: number
+// }
+
+// type Loc = {
+//     lat: number,
+//     long: number
+// }
+
+// let coordinates: Loc | Point = {x:1, y: 3}
+// coordinates = {lat:343.34, long: 234.343}
+
+function printAge(age: number | string): void {
+ console.log(`You are ${age} years old`)
 }
 
-type Loc = {
-    lat: number,
-    long: number
+function calculateTax(price: number | string, tax: number): number {
+    if (typeof price === 'string') {
+        price = parseFloat(price.replace('$', ''))
+    }
+    return price * tax
 }
 
-let coordinates: Loc | Point = {x:1, y: 3}
-coordinates = {lat:343.34, long: 234.343}
+console.log(calculateTax(23, 4))
+console.log(calculateTax('23', 4))
