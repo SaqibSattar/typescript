b42c067e51051fbc419a4e08911a8a978974df81
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-15 10:51:59 +0400

    alternative syntax of array types

--- Types/arrays.ts
+++ Types/arrays.ts
@@ -1,7 +1,19 @@
-const usersList = []
-usersList.push(2)
-usersList.push(2)
-const users: string[]  = ['user1', 'user2']
-users.push(1)
-users.push("1")
-const numbers: number[]  = [1, 2]
+// const usersList = []
+// usersList.push(2)
+// usersList.push(2)
+// const users: string[]  = ['user1', 'user2']
+// users.push(1)
+// users.push("1")
+// const numbers: number[]  = [1, 2]
+
+const usersList: Array<string> = []
+usersList.push('test')
+
+type Point = {
+    x: number,
+    y: number
+}
+
+const coords: Point[] = []
+coords.push({x:2, y:3})
+coords.push({x:2})
