d649972dd5df04c942c52394049a01f6b854421e
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-15 11:46:07 +0400

    union types and arrays

--- Types/unionTypes.ts
+++ Types/unionTypes.ts
@@ -16,16 +16,23 @@
 // let coordinates: Loc | Point = {x:1, y: 3}
 // coordinates = {lat:343.34, long: 234.343}
 
-function printAge(age: number | string): void {
- console.log(`You are ${age} years old`)
-}
+// function printAge(age: number | string): void {
+//  console.log(`You are ${age} years old`)
+// }
 
-function calculateTax(price: number | string, tax: number): number {
-    if (typeof price === 'string') {
-        price = parseFloat(price.replace('$', ''))
-    }
-    return price * tax
-}
+// function calculateTax(price: number | string, tax: number): number {
+//     if (typeof price === 'string') {
+//         price = parseFloat(price.replace('$', ''))
+//     }
+//     return price * tax
+// }
 
-console.log(calculateTax(23, 4))
-console.log(calculateTax('23', 4))
+// console.log(calculateTax(23, 4))
+// console.log(calculateTax('23', 4))
+
+const nums: number[] = [1,2,3]
+const stuff: any[] = ['test', 2]
+
+const stuff: (number | string)[] = [1,2,3,'test']
+const stuff: number | string[] = [1,2,3,'test']
+const stuff: number[] | string[] = [1,2,3,'test']
